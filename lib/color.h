7a4a951ad6bc4a6fdee7a626eba3bcb2b1a968f6
BrankoKrstic <brankokrstic94@gmail.com>
2025-04-21 01:43:52 +0200

    add gamma correction

--- lib/color.h
+++ lib/color.h
@@ -6,17 +6,30 @@
 
 using color = vec3;
 
+double linear_to_gamma(double linear_component)
+{
+  if (linear_component > 0.0)
+  {
+    return std::sqrt(linear_component);
+  }
+  return 0.0;
+}
+
 void write_color(std::ostream &out, const color &pixel_color)
 {
   auto r = pixel_color.x();
   auto g = pixel_color.y();
   auto b = pixel_color.z();
 
+  auto gr = linear_to_gamma(r);
+  auto gg = linear_to_gamma(g);
+  auto gb = linear_to_gamma(b);
+
   // Translate the [0,1] component values to the byte range [0,255].
   static const interval interval(0.0, 0.999);
-  int rbyte = int(256 * interval.clamp(r));
-  int gbyte = int(256 * interval.clamp(g));
-  int bbyte = int(256 * interval.clamp(b));
+  int rbyte = int(256 * interval.clamp(gr));
+  int gbyte = int(256 * interval.clamp(gg));
+  int bbyte = int(256 * interval.clamp(gb));
 
   // Write out the pixel color components.
   out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
